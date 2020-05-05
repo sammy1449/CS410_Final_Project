a03622154d08bd3956b83df185274f8455bb16b0
SamanthaMaxey299 <samanthamaxey299@onyx.boisestate.edu>
2020-05-04 22:34:47 -0600

    Java is ready

--- FinalProject.java
+++ FinalProject.java
@@ -17,13 +17,22 @@ you must create a classpath to reference (so that the java import jar file can b
 run the following command in onyx:
 here it is for your convenience:
 
-export CLASSPATH=/opt/mysql/mysql-connector-java-5.1.45/mysql-connector-java-5.1.45-bin.jar:$CLASSPATH
+export CLASSPATH=/opt/mysql/mysql-connector-java-8.0.18.jar:.:$CLASSPATH
 
 NOTE:  you must be on ONYX, and not a node for the 
 Compiler to find the classpath jar file
 */
 
+/**
+	This class runs the different procedures from mySQL Workbench. Each procedure is
+	unique from each other and runs a specific query on the data given.
+	@author Samantha Maxey & Aubrey Spannagel
+ */
 class project {
+
+	/**
+		Makes the connection to the mySQL database. Used in every procedure.
+	 */
 	public static Connection makeConnection(String port, String database, String password) {
 		try {
 			Connection conn = null;
@@ -44,6 +53,9 @@ class project {
 		return null;
 	}
 
+	/**
+		Lists the classes as well as number of students enrolled.
+	 */
 	public static void runlistclasses(Connection conn) {
 
 		PreparedStatement stmt = null;
@@ -60,7 +72,8 @@ class project {
 			}
 
 			// We have DATA!
-			rs.beforeFirst();  
+			rs.beforeFirst(); 
+			System.out.println("Class_ID, Course_Number, Term, Section_Number, COUNT(Student_ID)"); 
 			while (rs.next()) {  
       			// output must match result set columns
 				System.out.println(rs.getInt(1) 
@@ -95,6 +108,9 @@ class project {
 		}
 	}
 	
+	/**
+		Creates a new class in the Class table
+	 */
 	public static void runCreateClass(final Connection conn, final String course, final String semester, final String section, final String description) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -134,6 +150,10 @@ class project {
 		}
 	}
 	
+	/**
+		Selects a class as active using course number. Any other classes that are 
+		active will become inactive.
+	 */
 	public static void runSelectClass(Connection conn, String course) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -145,18 +165,6 @@ class project {
       		boolean isResultSet = stmt.execute();    
       		if(isResultSet) {
           			rs = stmt.getResultSet();
-			}
-
-			rs.beforeFirst();  
-			while (rs.next()) {  
-      			// output must match result set columns
-				System.out.println(rs.getInt(1) 
-						+ ":" + rs.getString(2) 
-						+ ":" + rs.getString(3) 
-						+ ":" + rs.getInt(4)
-						+ ":" + rs.getString(5)
-						+ ":" + rs.getString(6)
-						);
 			}
 
 		} catch (SQLException ex) {
@@ -184,6 +192,10 @@ class project {
 		}
 	}
 
+	/**
+		Selects a class as active using course and term. Any other classes 
+		that are active will become inactive.
+	 */
 	public static void runSelectClass2(Connection conn, String course, String term) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -196,18 +208,6 @@ class project {
       		boolean isResultSet = stmt.execute();    
       		if(isResultSet) {
           			rs = stmt.getResultSet();
-			}
-
-			rs.beforeFirst();  
-			while (rs.next()) {  
-      			// output must match result set columns
-				System.out.println(rs.getInt(1) 
-						+ ":" + rs.getString(2) 
-						+ ":" + rs.getString(3) 
-						+ ":" + rs.getInt(4)
-						+ ":" + rs.getString(5)
-						+ ":" + rs.getString(6)
-						);
 			}
 
 		} catch (SQLException ex) {
@@ -235,6 +235,10 @@ class project {
 		}
 	}
 	
+	/**
+		Selects a class as active using course, term and section. Any other classes that are 
+		active will become inactive.
+	 */
 	public static void runSelectClass3(Connection conn, String course, String term, String number) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -252,17 +256,7 @@ class project {
 			}
 
 			// We have DATA!
-			rs.beforeFirst();  
-			while (rs.next()) {  
-      			// output must match result set columns
-				System.out.println(rs.getInt(1) 
-						+ ":" + rs.getString(2) 
-						+ ":" + rs.getString(3) 
-						+ ":" + rs.getInt(4)
-						+ ":" + rs.getString(5)
-						+ ":" + rs.getString(6)
-						);
-			}
+			
 		} catch (SQLException ex) {
 			// handle any errors
 			System.err.println("SQLException: " + ex.getMessage());
@@ -288,6 +282,9 @@ class project {
 		}
 	}
 	
+	/**
+		Shows the current active class
+	 */
 	public static void runShowClass(Connection conn) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -304,14 +301,10 @@ class project {
 
 			// We have DATA!
 			rs.beforeFirst();  
+			System.out.println("Course_Number");
 			while (rs.next()) {  
       			// output must match result set columns
-				System.out.println(rs.getInt(1) 
-						+ ":" + rs.getString(2) 
-						+ ":" + rs.getString(3) 
-						+ ":" + rs.getInt(4)
-						+ ":" + rs.getString(5)
-						+ ":" + rs.getString(6)
+				System.out.println(rs.getString(1) 
 						);
 			}
 		} catch (SQLException ex) {
@@ -339,6 +332,9 @@ class project {
 		}
 	}
 	
+	/**
+		Shows all the Assignment Categories in the current active class
+	 */
 	public static void runShowCategories(Connection conn) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -353,7 +349,8 @@ class project {
 			}
 
 			// We have DATA!
-			rs.beforeFirst();  
+			rs.beforeFirst(); 
+			System.out.println("Category_Name, Weight, Class_ID"); 
 			while (rs.next()) {  
       			// output must match result set columns
 				System.out.println(rs.getString(1) 
@@ -386,6 +383,9 @@ class project {
 		}
 	}
 	
+	/**
+		Adds an assignment category to the current active class
+	 */
 	public static void runAddCategory(Connection conn, String Category, String weight) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -427,6 +427,9 @@ class project {
 		}
 	}
 	
+	/**
+		Shows the assignments and total points possible in the current active class
+	 */
 	public static void runShowAssignment(Connection conn) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -441,6 +444,7 @@ class project {
 			}
 
 			rs.beforeFirst();  
+			System.out.println("Assignment_Name, Points, Category_Name");
 			while (rs.next()) {  
       			// output must match result set columns
 				System.out.println(rs.getString(1) 
@@ -474,6 +478,9 @@ class project {
 		}
 	}
 	
+	/**
+		Adds an assignment to the current active class
+	 */
 	public static void runAddAssignment(Connection conn, String assignment, String cat_name, String a_des, String points) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -516,6 +523,9 @@ class project {
 		}
 	}
 	
+	/**
+		Adds a new student to the active class
+	 */
 	public static void runAddStudent(Connection conn, String uname, String s_id, String LName, String FName) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -557,6 +567,9 @@ class project {
 		}
 	}
 	
+	/**
+		Adds a student already created to the current active class
+	 */
 	public static void runAddStudent2(Connection conn, String uname) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -595,6 +608,9 @@ class project {
 		}
 	}
 	
+	/**
+		Shows all the students in the current active class
+	 */
 	public static void runShowStudents(Connection conn) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -609,13 +625,14 @@ class project {
 			}
 
 			rs.beforeFirst();  
+			System.out.println("Student_ID, First_Name, Last_Name, Username, Course_Number");
 			while (rs.next()) {  
       			// output must match result set columns
 				System.out.println(rs.getInt(1) 
 						+ ":" + rs.getString(2) 
 						+ ":" + rs.getString(3)
 						+ ":" + rs.getString(4)
-						+ ":" + rs.getInt(5)
+						+ ":" + rs.getString(5)
 						);
 			}
 
@@ -644,6 +661,10 @@ class project {
 		}
 	}
 
+	/**
+		Shows students currently in the class with the string contained in their name or
+		username
+	 */
 	public static void runShowStudents2(Connection conn, String str) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -659,11 +680,13 @@ class project {
 			}
 
 			rs.beforeFirst();  
+			System.out.println("Student_ID, First_Name, Last_name, Username");
 			while (rs.next()) {  
       			// output must match result set columns
-				System.out.println(rs.getString(1) 
+				System.out.println(rs.getInt(1) 
 						+ ":" + rs.getString(2) 
 						+ ":" + rs.getString(3)
+						+ ":" + rs.getString(4)
 						);
 			}
 
@@ -691,7 +714,14 @@ class project {
 			}
 		}
 	}
-
+	
+	/**
+		Grade is calculated here in mySQL. It pulls assignments, students, and points
+		students earn with those assignments and gives the student's grade. It then
+		either updates the gradebook or inserts a new grade into the gradebook. It also
+		checks for if the total points earned are greater than the total points of the
+		assignment and gives a warning if the points earned is exceeding.
+	 */
 	public static void runGrade(Connection conn, String assignment_name, String uName, String grade) {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
@@ -708,12 +738,59 @@ class project {
           			rs = stmt.getResultSet();
 			}
 
-			rs.beforeFirst();  
+		} catch (SQLException ex) {
+			// handle any errors
+			System.err.println("SQLException: " + ex.getMessage());
+			System.err.println("SQLState: " + ex.getSQLState());
+			System.err.println("VendorError: " + ex.getErrorCode());
+		} finally {
+			// it is a good idea to release resources in a finally{} block
+			// in reverse-order of their creation if they are no-longer needed
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				rs = null;
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				stmt = null;
+			}
+		}
+	}
+
+	/**
+		Shows the grades of a single student in the current active class
+	 */
+	public static void runStudentGrade(Connection conn, String username) {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		try {
+				// be sure to call the appropriate procedure based on the function youre running
+			stmt = conn.prepareStatement("call student_grades(?)");
+			stmt.setString(1, username);
+			
+			
+      		boolean isResultSet = stmt.execute();    
+      		if(isResultSet) {
+          			rs = stmt.getResultSet();
+			}
+
+			rs.beforeFirst(); 
+			System.out.println("Username, Assignment_grade, Assignment_Name, Points_Earned, Points_Possible, Category_Name"); 
 			while (rs.next()) {  
       			// output must match result set columns
 				System.out.println(rs.getString(1) 
-						+ ":" + rs.getInt(2) 
+						+ ":" + rs.getDouble(2) 
 						+ ":" + rs.getString(3)
+						+ ":" + rs.getInt(4)
+						+ ":" + rs.getInt(5)
+						+ ":" + rs.getString(6)
 						);
 			}
 
@@ -742,11 +819,68 @@ class project {
 		}
 	}
 
+	/**
+		Shows the total grade of every student currently enrolled in the class by
+		dividing the sum of the total points earned by the sum of the total points 
+		possible.
+	 */
+	public static void runGradebook(Connection conn) {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		try {
+				// be sure to call the appropriate procedure based on the function youre running
+			stmt = conn.prepareStatement("call gradebook()");
+			
+      		boolean isResultSet = stmt.execute();    
+      		if(isResultSet) {
+          			rs = stmt.getResultSet();
+			}
+
+			rs.beforeFirst();  
+			System.out.println("Student_ID, Last_Name, Username, Total_Points_Earned, Grade");
+			while (rs.next()) {  
+      			// output must match result set columns
+				System.out.println(rs.getInt(1) 
+						+ ":" + rs.getString(2) 
+						+ ":" + rs.getString(3)
+						+ ":" + rs.getInt(4)
+						+ ":" + rs.getDouble(5)
+						);
+			}
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.err.println("SQLException: " + ex.getMessage());
+			System.err.println("SQLState: " + ex.getSQLState());
+			System.err.println("VendorError: " + ex.getErrorCode());
+		} finally {
+			// it is a good idea to release resources in a finally{} block
+			// in reverse-order of their creation if they are no-longer needed
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				rs = null;
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				stmt = null;
+			}
+		}
+	}
+
+	/**
+		Main method. Runs a usage statment with '/?' as well as checks for the 
+		first argument to call one of the methods above.
+	 */
 	public static void main(String[] args) {
 		try {
-			// The newInstance() call is a work around for some broken Java implementations
-			
-			
+			//Usage statements for first time users
 			if (args[0].equals("/?") ){
 			  System.out.println ("Usage :   new-class <CourseNumber> <Term> <Section> <Description>");
 			  System.out.println ("Usage :   list-classes");
@@ -756,13 +890,15 @@ class project {
 			  System.out.println ("Usage :   show-class");
 			  System.out.println ("Usage :   show-categories");
 			  System.out.println ("Usage :   add-category <CatName> <Weight>");
-			  System.out.println ("Usage :   show-assignment");
+			  System.out.println ("Usage :   show-assignments");
 			  System.out.println ("Usage :   add-assignment <AName> <Category> <Description> <Points>");
 			  System.out.println ("Usage :   add-student <Username> <sID> <LastN> <FirstN>");
 			  System.out.println ("Usage :   add-student2 <Username>");
 			  System.out.println ("Usage :   show-students");
 			  System.out.println ("Usage :   show-students2 <String>");
 			  System.out.println ("Usage :   grade <AName> <Username> <Grade>");
+			  System.out.println ("Usage :   student_grades <Username>");
+			  System.out.println ("Usage :   gradebook");
 			  return;
 			}
 			else {
@@ -779,10 +915,6 @@ class project {
 			System.out.println();
 			System.out.println("JDBC driver loaded");
     Connection conn = makeConnection("57036", "Final","Nightwing0225");
-
-// TO DO:  add to the code here to run the appropriate function
-// when the getdata request is made 
-// at the command line
 
 			if (args.length >= 1 && args.length <= 6)
 			{
@@ -831,20 +963,28 @@ class project {
 			 		runAddStudent(conn, args[1], args[2], args[3], args[4]);
 			 	}
 				else if(args[0].equals("add-student2")){
-					System.out.println("Running add-student2")
-					runAddStudent2(conn, args[1])
+					System.out.println("Running add-student2");
+					runAddStudent2(conn, args[1]);
 				}
 				else if(args[0].equals("show-students")){
-					System.out.println("Running show-students")
-					runShowStudents(conn)
+					System.out.println("Running show-students");
+					runShowStudents(conn);
 				}
 				else if(args[0].equals("show-students2")){
-					System.out.println("Running show-students2")
-					runShowStudents2(conn, args[1])
+					System.out.println("Running show-students2");
+					runShowStudents2(conn, args[1]);
 				}
 				else if(args[0].equals("grade")){
-					System.out.println("Running grade")
-					runGrade(conn, args[1], args[2], args[3])
+					System.out.println("Running grade");
+					runGrade(conn, args[1], args[2], args[3]);
+				}
+				else if(args[0].equals("student-grades")){
+					System.out.println("Running student-grades");
+					runStudentGrade(conn, args[1]);
+				}
+				else if(args[0].equals("gradebook")){
+					System.out.println("Running gradebook");
+					runGradebook(conn);
 				}
 			}
 			else if (args[0].equals("/?") ){
